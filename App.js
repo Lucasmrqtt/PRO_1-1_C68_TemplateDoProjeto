1fa62ee8c05db01275919b7e7ebd1dfa581f1bc8
Lucasmrqtt <marquettilu@gmail.com>
2023-03-06 21:10:24 -0300

    Projeto 68

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import BottomTabNavigator from "./components/BottomTabNavigator";
 
 export default class App extends Component {
   render() {
-
-    //return <BottomTabNavigator>;
-    //<BottomTabNavigator/>
-    //return <BottomTabNavigator />;
-    //return <BottomTabNavigator <BottomTabNavigator> />;
-
+    return(
+      <BottomTabNavigator/>
+      //return <BottomTabNavigator>;
+      //<BottomTabNavigator/>
+      //return <BottomTabNavigator <BottomTabNavigator> />;
+    )
   }
 }
